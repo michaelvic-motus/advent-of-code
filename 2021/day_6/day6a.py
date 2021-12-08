0667ca7f3ec97934ed0ce0ca1a9626a38ca2b980
Michael <michaeltv.sa@gmail.com>
2021-12-08 11:36:20 +0200

    updates to day 6

--- 2021/day_6/day6a.py
+++ 2021/day_6/day6a.py
@@ -5,3 +5,27 @@
 
 with open('input.txt') as f:
     data = f.readlines()
+data = data[0].split(',')
+data = [int(x) for x in data]
+
+
+# Loop over list
+days = 80
+
+for day in range(1, days + 1):
+    print(day)
+    appendCnt = 0
+    for idx, timer in enumerate(data):
+        if timer == 0:
+            data[idx] = 6
+            appendCnt += 1    
+        else:
+            data[idx] = timer - 1
+        
+    for newFish in range(appendCnt):
+        data.append(8)
+
+    #print('day:', day, '|', data)
+
+print('answer', len(data))
+ 
